e670fd49b55ef571d904c66025519bf42cc70b2a
followyaya <followyaya@gmail.com>
2026-02-04 06:56:15 -0500

    chore: remove XCUBES from featured projects

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -65,14 +65,6 @@ export default function Home() {
       link: "https://edkempire.vercel.app/",
     },
     {
-      title: "XCUBES",
-      description: "Maisons Conteneurs Écologiques - Solutions modulaires, commerciales et personnalisable avec garantie jusqu'à 5 ans",
-      image: "https://api.microlink.io/?url=https://xcubes.vercel.app/&screenshot=true&meta=false&embed=screenshot.url",
-      tech: ["Next.js", "React", "Tailwind CSS"],
-      link: "https://xcubes.vercel.app/",
-    },
-  
-    {
       title: "Senparc Dakar",
       description: "Le Spot Fun pour Petits et Grands - Parc pour enfants sécurisé, trampolines, jeux gonflables, restaurant & gourmandises",
       image: "https://api.microlink.io/?url=https://senparcdakar.vercel.app/&screenshot=true&meta=false&embed=screenshot.url",
